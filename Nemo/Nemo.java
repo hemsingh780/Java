05cb249faf81c2c3165b1e6dab3632da53f459e0
Your Name <you@example.com>
2021-02-07 08:10:26 +0530

    add time complexcity program

---
+++ Nemo/Nemo.java
@@ -0,0 +1,20 @@
+public class Nemo{	
+    static String array(String a[]) {
+        for (int i = 0; i <= 1; i++) {
+            if (a[i] == "nemo") {
+                return "nemo is here";
+            }
+        }
+        return "Nemo is not here";
+    }
+
+    public static void main(String[] args) {
+        String b[] = { "nemo" };
+        long startTime = System.nanoTime();
+        long endTime = System.nanoTime();
+        long timeElapsed = endTime - startTime;
+        System.out.println(array(b));
+        System.out.println("Execution time in milliseconds : " + timeElapsed / 1000000);
+
+    }
+}
